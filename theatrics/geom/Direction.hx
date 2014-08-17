f53fd2fb7081d78be842c4107b877f017caf56cd
Nycto <James@RoundEights.com>
2014-08-17 16:16:52 -0700

    Add geom.Direction

---
+++ theatrics/geom/Direction.hx
@@ -0,0 +1,16 @@
+package theatrics.geom;
+
+/**
+ * The directions on a compass. Useful for grid based games.
+ */
+enum Direction {
+    North;
+    NorthEast;
+    East;
+    SouthEast;
+    South;
+    SouthWest;
+    West;
+    NorthWest;
+}
+
